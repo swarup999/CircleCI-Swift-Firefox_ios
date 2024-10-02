566b0f21610a76e728f2b8af4ea58760fbaef205
Clare So <cso@mozilla.com>
2024-10-02 13:31:25 -0400

    For fun: Can we do BrowserKit first?

--- checkout.sh
+++ checkout.sh
@@ -5,6 +5,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 set -x
+
+(cd BrowserKit && swift run)
+
 cd focus-ios
 
 # Version 107.0 hash
@@ -17,5 +20,3 @@ curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/mozilla/
 # Clone shavar prod list
 cd .. # Make sure we are at the root of the repo
 rm -rf shavar-prod-lists && git clone https://github.com/mozilla-services/shavar-prod-lists.git && git -C shavar-prod-lists checkout $SHAVAR_COMMIT_HASH
-
-(cd BrowserKit && swift run)
